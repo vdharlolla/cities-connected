52a111c8e6a4b73b109d0604a9303094b6de8081
vamsi lolla <vlolla-consultant@scholastic.com>
2020-09-21 20:45:10 -0400

    modified additional logic

--- src/main/java/com/citiesconnected/service/CitiesConnectionServiceImpl.java
+++ src/main/java/com/citiesconnected/service/CitiesConnectionServiceImpl.java
@@ -1,11 +1,5 @@
 package com.citiesconnected.service;
 
-import java.util.ArrayDeque;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
@@ -33,24 +27,7 @@ public class CitiesConnectionServiceImpl implements CitiesConnectionService {
 		if (destination.connectedCities().contains(origin)) {
 			return YES;
 		}
-		
-		Set<City> originCity = new HashSet<>(Collections.singleton(origin));
-
-		Deque<City> citiesList = new ArrayDeque<>(origin.connectedCities());
-
-		while (!citiesList.isEmpty()) {
-			City city = citiesList.getLast();
-			if (city.equals(destination))
-				return YES;
-			if (!originCity.contains(city)) {
-				originCity.add(city);
-				citiesList.addAll(city.connectedCities());
-				citiesList.removeAll(originCity);
-			} else {
-				citiesList.removeAll(Collections.singleton(city));
-			}
-		}
-
+	
 		return NO;
 	}
 }
